a5450d0d5b5a34c00e1660201c4740f32ed86c19
Shantanu <64133232+Shantanu-verma-sys@users.noreply.github.com>
2021-01-12 16:27:00 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,42 +1,15 @@
-import json
-import websocket
+from webdata import *
 
-time_interval = "4h"
 
-def onopen(con):
-    print("connection open")
-    x = json.dumps({
-      "method": "SUBSCRIBE",
-      "params": [
-        "btcusdt@kline_"+time_interval,
-      ],
-      "id": 1
-    })
-    con.send(x)
-
-def onErr(con,err):
-    print(err)
-
-def onMes(con,m):
-    val = json.loads(m)["data"]
-    p = {"s":"Symbol ",
-         "i":"Interval ",
-         "o":"Open Price ",
-         "c":"Close Price ",
-         "h":"High Price ",
-         "l":"Low Price ",
-         "x":"Closed? "
-         }
+def on_data(m):
+    '''Main algo here'''
+    print(m)
     
-    for i in val["k"]:
-        if i in p:
-            print(p[i]+" : "+str(val["k"][i]))
-    print("-"*20)
-        
+if __name__ == "__main__":
 
-def onClose(con):
-    print("con closed")
-
-con = websocket.WebSocketApp("wss://stream.binance.com:9443/stream?streams=kline_"+time_interval,on_message = onMes,on_error = onErr,on_close = onClose)
-con.on_open = onopen
-con.run_forever()
+    interval = '4h'
+    coin_name = 'btc'
+    coin_vs_name = 'usdt'
+    coin_pair = coin_name + coin_vs_name
+    
+    F = FetchData(interval,coin_pair,on_data)
